239ce4a2f4ca7302b970639243b33559d25364ab
zwen-dudu <2062520350@qq.com>
2024-07-23 10:34:11 +0800

    Update ackhandler.go

--- internal/ackhandler/ackhandler.go
+++ internal/ackhandler/ackhandler.go
@@ -1,9 +1,9 @@
 package ackhandler
 
 import (
-	"github.com/quic-go/quic-go/internal/protocol"
-	"github.com/quic-go/quic-go/internal/utils"
-	"github.com/quic-go/quic-go/logging"
+	"github.com/zwen-dudu/quic-go/internal/protocol"
+	"github.com/zwen-dudu/quic-go/internal/utils"
+	"github.com/zwen-dudu/quic-go/logging"
 )
 
 // NewAckHandler creates a new SentPacketHandler and a new ReceivedPacketHandler.
